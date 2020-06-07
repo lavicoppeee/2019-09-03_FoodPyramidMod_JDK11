5d55d6474d72673ec28af65267b67dddf983579a
s244926 <s244926@studenti.polito.it>
2020-06-08 01:23:09 +0200

    Progetto Base

---
+++ src/main/java/it/polito/tdp/food/FXMLController.java
@@ -0,0 +1,75 @@
+package it.polito.tdp.food;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.food.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+	
+	private Model model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtCalorie;
+
+    @FXML
+    private TextField txtPassi;
+
+    @FXML
+    private Button btnAnalisi;
+
+    @FXML
+    private Button btnCorrelate;
+
+    @FXML
+    private Button btnCammino;
+
+    @FXML
+    private ComboBox<?> boxPorzioni;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doCammino(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCorrelate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCreaGrafo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtCalorie != null : "fx:id=\"txtCalorie\" was not injected: check your FXML file 'Food.fxml'.";
+        assert txtPassi != null : "fx:id=\"txtPassi\" was not injected: check your FXML file 'Food.fxml'.";
+        assert btnAnalisi != null : "fx:id=\"btnAnalisi\" was not injected: check your FXML file 'Food.fxml'.";
+        assert btnCorrelate != null : "fx:id=\"btnCorrelate\" was not injected: check your FXML file 'Food.fxml'.";
+        assert btnCammino != null : "fx:id=\"btnCammino\" was not injected: check your FXML file 'Food.fxml'.";
+        assert boxPorzioni != null : "fx:id=\"boxPorzioni\" was not injected: check your FXML file 'Food.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Food.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+}
